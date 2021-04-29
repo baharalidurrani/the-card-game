465ae050da36617d4007dae775686a4cb6210001
baharalidurrani <baharalidurrani@gmail.com>
2021-04-29 12:14:54 +0500

    npm run lint

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import React, { useState } from "react";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import styles from "./Counter.module.css";
 import {
   decrement,
   increment,
@@ -8,13 +8,12 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
 
 export function Counter() {
   const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   const incrementValue = Number(incrementAmount) || 0;
 
