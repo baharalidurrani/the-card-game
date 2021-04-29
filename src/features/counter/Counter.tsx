8cc1a9de893c3dd033ffc3059640fabde521882c
baharalidurrani <baharalidurrani@gmail.com>
2021-04-30 02:13:00 +0500

    CSS removed

--- src/features/counter/Counter.tsx
+++ src/features/counter/Counter.tsx
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
+import { Button, TextField, Typography } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import RemoveCircleOutlineOutlinedIcon from "@material-ui/icons/RemoveCircleOutlineOutlined";
 import { useAppSelector, useAppDispatch } from "../../app-redux/hooks";
-import styles from "./Counter.module.css";
 import {
   decrement,
   increment,
@@ -18,47 +24,66 @@ export function Counter() {
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
+    <Container>
+      <Box m={10}>
+        <Grid container direction="row" justify="space-evenly" alignItems="center">
+          <Grid item>
+            <IconButton
+              color="primary"
+              aria-label="Decrement value"
+              onClick={() => dispatch(decrement())}
+            >
+              <RemoveCircleOutlineOutlinedIcon />
+            </IconButton>
+          </Grid>
+          <Grid item>
+            <Typography variant="h3">{count}</Typography>
+          </Grid>
+          <Grid item>
+            <IconButton
+              color="primary"
+              aria-label="Increment value"
+              onClick={() => dispatch(increment())}
+            >
+              <AddCircleOutlineOutlinedIcon />
+            </IconButton>
+          </Grid>
+        </Grid>
+      </Box>
+      <Grid container direction="row" justify="space-evenly" alignItems="center">
+        <TextField
+          type="number"
+          size="small"
+          label="Set increment amount"
+          variant="outlined"
           value={incrementAmount}
           onChange={(e) => setIncrementAmount(e.target.value)}
         />
-        <button
-          className={styles.button}
+        <Button
+          color="primary"
+          variant="outlined"
+          size="large"
           onClick={() => dispatch(incrementByAmount(incrementValue))}
         >
           Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
+        </Button>
+        <Button
+          color="primary"
+          variant="outlined"
+          size="large"
           onClick={() => dispatch(incrementAsync(incrementValue))}
         >
           Add Async
-        </button>
-        <button className={styles.button} onClick={() => dispatch(incrementIfOdd(incrementValue))}>
+        </Button>
+        <Button
+          color="primary"
+          variant="outlined"
+          size="large"
+          onClick={() => dispatch(incrementIfOdd(incrementValue))}
+        >
           Add If Odd
-        </button>
-      </div>
-    </div>
+        </Button>
+      </Grid>
+    </Container>
   );
 }
