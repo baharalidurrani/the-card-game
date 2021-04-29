b8a444ed248bf93d0b7972570c1f5e9fc712c56b
baharalidurrani <baharalidurrani@gmail.com>
2021-04-29 16:55:33 +0500

    counter route added

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,15 @@
+import DialpadIcon from "@material-ui/icons/Dialpad";
 import HomeIcon from "@material-ui/icons/Home";
 import InfoIcon from "@material-ui/icons/Info";
 import NewReleasesIcon from "@material-ui/icons/NewReleases";
 import { About } from "../components/About/About";
 import { Home } from "../components/Home/Home";
 import { LongText } from "../components/Placeholder/LongText";
+import { Counter } from "../features/counter/Counter";
 
 export const ROUTES = [
   { name: "Home", path: "/", component: Home, icon: HomeIcon },
   { name: "Long Text", path: "/long", component: LongText, icon: NewReleasesIcon },
+  { name: "Counter", path: "/counter", component: Counter, icon: DialpadIcon },
   { name: "About", path: "/about", component: About, icon: InfoIcon },
 ];
