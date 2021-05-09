ef0dddfcf84a01bc29e8a0c3474a712b9e47a5ef
baharalidurrani <baharalidurrani@gmail.com>
2021-05-10 03:57:50 +0500

    box added

--- src/components/organisms/Details.tsx
+++ src/components/organisms/Details.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
 import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
@@ -19,13 +20,16 @@ export function Details({ wrapText, data, selected, changeSelect }: Props) {
       }}
     >
       <CardContent>
-        {!wrapText && <Typography variant="h4">Details</Typography>}
-        <br />
-        <Typography noWrap={wrapText}>{data.realName}</Typography>
-        <br />
-        <Typography noWrap={wrapText}>{data.playerName}</Typography>
-        <br />
-        <Typography noWrap={wrapText}>{data.asset}</Typography>
+        <Box m={1}>{!wrapText && <Typography variant="h4">Details</Typography>}</Box>
+        <Box m={1}>
+          <Typography noWrap={wrapText}>{data.realName}</Typography>
+        </Box>
+        <Box m={1}>
+          <Typography noWrap={wrapText}>{data.playerName}</Typography>
+        </Box>
+        <Box m={1}>
+          <Typography noWrap={wrapText}>{data.asset}</Typography>
+        </Box>
       </CardContent>
     </Card>
   );
